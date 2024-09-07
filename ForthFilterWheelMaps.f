6e6d9790e4bf22a1120ddcddda776ebf9d5ad23a
Andrew Read <andrew81244@outlook.com>
2024-09-07 23:27:32 +0800

    Refactored ForthFilterWheel - needs testing

---
+++ ForthFilterWheelMaps.f
@@ -0,0 +1,67 @@
+\ Add FITS and XISF keywords to a map
+\ 	ASI version
+
+\ requires forthbase.f
+\ requires EFW_SDK.f
+\ requires EFW_SDK_extend.f
+\ requires maps.fs
+\ requires map-tools.fs
+
+\ FITS keywords supported by MaxImDL
+\ ==================================
+\ FILTER		- filter name
+
+\ FITS keywords defined here for the ZWO wheel
+\ =============================================
+\ WHEEL		- name of the filter wheel
+\ WHEELPOS	- position of the filter wheel
+\ WHEELSN 	- wheel serial number
+\ FILTERSP	- filter specification
+
+begin-enum
+	+enum LUM
+	+enum RED
+	+enum GREEN
+	+enum BLUE
+	+enum H_alpha
+	+enum SII
+	+enum OIII
+end-enum
+
+: FITSfilterBand ( n -- caddr u)
+	case
+	LUM 		of s" Luminescence" endof
+	RED 		of s" RED" endof
+	GREEN 	of s" GREEN" endof
+	BLUE 		of s" BLUE" endof
+	SII 		of s" H_alpha" endof
+	H_alpha 	of s" SII" endof
+	OIII 		of s" OIII" endof
+	." " rot endcase
+;
+
+: FITSfilterSpec ( n -- caddr u)
+	case
+	LUM 		of s" Astronomik Deep Sky II" endof
+	RED 		of s" Astronomik Deep Sky II" endof
+	GREEN 	of s" Astronomik Deep Sky II" endof
+	BLUE 		of s" Astronomik Deep Sky II" endof
+	SII 		of s" Astronomik 3nm" endof
+	H_alpha 	of s" Astronomik 3nm" endof
+	OIII 		of s" Astronomik 3nm" endof
+	." " rot endcase
+;
+
+: add-wheelFITS ( map --)
+\ add key value pairs for FITS wheel parameters
+	>R
+	s" FILTER" wheel_position FITSfilterBand 		R@ =>" FILTER"
+	s" FILTERSP" wheel_position FITSfilterSpec 	R@ =>" FILTERSP"	
+	wheel_name				R@ =>" WHEEL"
+	wheel_position (.) 	R@ =>" WHEELPOS"
+	wheel_SN					R@ =>" WHEELSN"
+	R> drop
+;	
+
+	
+
---
+++ ForthFilterWheelsMaps_test1.f
@@ -0,0 +1,46 @@
+\ test for ForthAstroCameraFITS.f
+
+include "%idir%\..\..\ForthBase\ForthBase.f"
+include "%idir%\EFW_SDK.f"
+include "%idir%\EFW_SDK_extend.f"
+include "%idir%\ForthFilterWheel.f"
+include "%idir%\..\..\ForthBase\serial\VFX32serial.f"
+include "%idir%\..\..\ForthKMTronic\KMTronic_Bidmead.f"
+include "%idir%\..\..\ForthKMTronic\KMTronic.f"
+include "%idir%\..\..\forth-map\map.fs"
+include "%idir%\..\..\forth-map\map-tools.fs"
+include "%idir%\ForthFilterWheelMaps.f"
+
+-1 constant power-is-relay-switched
+CR
+
+power-is-relay-switched [IF] 
+\ Switch on the camera relay
+
+	COM-KMT add-relays
+	1 relay-on
+	3000 ms
+	." Relay power on" CR
+[THEN]
+
+scan-wheels
+0 add-wheel
+0 use-wheel
+500 ms
+
+	map-strings
+	map CONSTANT wheel_FITSmap
+	wheel_FITSmap add-wheelFITS
+	wheel_FITSmap .map
+
+
+power-is-relay-switched [IF]
+\ Switch off the camera relay
+
+	500 ms
+	1 relay-off
+	CR ." Relay power off" CR
+
+	remove-relays
+	
+[THEN]
